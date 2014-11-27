ff84561aad17f14299a0a6509540f8ad6248c5da
david <david@allthingsdigital.nl>
2014-11-27 07:01:13 +0100

    Minor code cleanup.

--- src/main/java/org/molgenis/byod/rdf/ParticipantEntityMetadata.java
+++ src/main/java/org/molgenis/byod/rdf/ParticipantEntityMetadata.java
@@ -11,61 +11,64 @@ import com.hp.hpl.jena.sparql.vocabulary.FOAF;
 import com.hp.hpl.jena.vocabulary.DCTerms;
 import com.hp.hpl.jena.vocabulary.RDFS;
 
-public class ParticipantEntityMetadata implements MetaData
-{
-	public static final String PARTICIPANT_ID = "participantID";
-	public static final String DIAGNOSIS_CODE = "diagnosis_code";
-	public static final String ETHNICITY = "ethnicity";
-	public static final String GENDER = "gender";
+public class ParticipantEntityMetadata implements MetaData {
 
-	private static final List<String> columns = Arrays.asList(PARTICIPANT_ID, DIAGNOSIS_CODE, ETHNICITY, GENDER);
+    public static final String PARTICIPANT_ID = "participantID";
+    public static final String DIAGNOSIS_CODE = "diagnosis_code";
+    public static final String ETHNICITY = "ethnicity";
+    public static final String GENDER = "gender";
 
-	private Map<String, String> attributeToOntologyUri;
-	{
-		attributeToOntologyUri = new HashMap<String, String>();
-		attributeToOntologyUri.put(DIAGNOSIS_CODE, RdfExporter.NCI_PREFIX);
-		attributeToOntologyUri.put(PARTICIPANT_ID, RdfExporter.CATALOG_PARTICIPANT_ID_PREFIX);
-	}
+    private static final List<String> columns = Arrays.asList(PARTICIPANT_ID, DIAGNOSIS_CODE, ETHNICITY, GENDER);
 
-	private Map<String, Property> attributeProperty;
-	{
-		attributeProperty = new HashMap<String, Property>();
-		attributeProperty.put(DIAGNOSIS_CODE, RDFS.seeAlso);
-		attributeProperty.put(ETHNICITY, DCTerms.identifier);
-		attributeProperty.put(GENDER, FOAF.gender);
-	}
+    private final Map<String, String> attributeToOntologyUri;
 
-	private Map<String, XSDDatatype> attrToDataType;
-	{
-		attrToDataType = new HashMap<String, XSDDatatype>();
-		attrToDataType.put(PARTICIPANT_ID, XSDDatatype.XSDstring);
-		attrToDataType.put(DIAGNOSIS_CODE, XSDDatatype.XSDstring);
-		attrToDataType.put(ETHNICITY, XSDDatatype.XSDstring);
-		attrToDataType.put(GENDER, XSDDatatype.XSDstring);
-	}
+    {
+        attributeToOntologyUri = new HashMap<>();
+        attributeToOntologyUri.put(DIAGNOSIS_CODE, RdfExporter.NCI_PREFIX);
+        attributeToOntologyUri.put(PARTICIPANT_ID, RdfExporter.CATALOG_PARTICIPANT_ID_PREFIX);
+    }
 
-	public XSDDatatype getDataType(String attributeName)
-	{
-		return attrToDataType.containsKey(attributeName) ? attrToDataType.get(attributeName) : XSDDatatype.XSDstring;
-	}
+    private final Map<String, Property> attributeProperty;
 
-	public String getOntologyPrefix(String attributeName)
-	{
-		return attributeToOntologyUri.containsKey(attributeName) ? attributeToOntologyUri.get(attributeName) : null;
-	}
+    {
+        attributeProperty = new HashMap<>();
+        attributeProperty.put(DIAGNOSIS_CODE, RDFS.seeAlso);
+        attributeProperty.put(ETHNICITY, DCTerms.identifier);
+        attributeProperty.put(GENDER, FOAF.gender);
+    }
 
-	public Property getAttrProperty(String attributeName)
-	{
-		return attributeProperty.containsKey(attributeName) ? attributeProperty.get(attributeName) : null;
-	}
+    private final Map<String, XSDDatatype> attrToDataType;
 
-	public String getIdentifierAttr()
-	{
-		return PARTICIPANT_ID;
-	}
+    {
+        attrToDataType = new HashMap<>();
+        attrToDataType.put(PARTICIPANT_ID, XSDDatatype.XSDstring);
+        attrToDataType.put(DIAGNOSIS_CODE, XSDDatatype.XSDstring);
+        attrToDataType.put(ETHNICITY, XSDDatatype.XSDstring);
+        attrToDataType.put(GENDER, XSDDatatype.XSDstring);
+    }
 
-	public List<String> getAttributeNames()
-	{
-		return columns;
-	}
+    @Override
+    public XSDDatatype getDataType(String attributeName) {
+        return attrToDataType.containsKey(attributeName) ? attrToDataType.get(attributeName) : XSDDatatype.XSDstring;
+    }
+
+    @Override
+    public String getOntologyPrefix(String attributeName) {
+        return attributeToOntologyUri.get(attributeName);
+    }
+
+    @Override
+    public Property getAttrProperty(String attributeName) {
+        return attributeProperty.get(attributeName);
+    }
+
+    @Override
+    public String getIdentifierAttr() {
+        return PARTICIPANT_ID;
+    }
+
+    @Override
+    public List<String> getAttributeNames() {
+        return columns;
+    }
 }
--- src/main/java/org/molgenis/byod/rdf/SampleEntityMetadata.java
+++ src/main/java/org/molgenis/byod/rdf/SampleEntityMetadata.java
@@ -9,81 +9,84 @@ import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.vocabulary.RDFS;
 
-public class SampleEntityMetadata implements MetaData
-{
-	public static final String SAMPLE_ID = "specimenID";
-	public static final String SAMPLE_TYPE_CODE = "specimenType_code";
-	public static final String ANATOMIC_SOURCE_CODE = "anatomicSource_code";
-	public static final String LONG_TERM_STROAGE_CONTAINER = "longTermStorageContainer";
-	public static final String STORAGE_TEMPERATURE = "storageTemperature";
-	public static final String PRESERVATION_TYPE_CODE = "preservationType_code";
-	public static final String PARTICIPANT_AGE_UNIT = "participantAge_unit";
-	public static final String PARTICIPANT_AGE = "participantAge_value";
-	public static final String SPECIMEN_COUNT = "specimenCount";
+public class SampleEntityMetadata implements MetaData {
 
-	private static final List<String> columns = Arrays.asList(SAMPLE_ID, SAMPLE_TYPE_CODE, SAMPLE_TYPE_CODE,
-			LONG_TERM_STROAGE_CONTAINER, STORAGE_TEMPERATURE, PRESERVATION_TYPE_CODE, PARTICIPANT_AGE_UNIT,
-			PARTICIPANT_AGE, SPECIMEN_COUNT);
+    public static final String SAMPLE_ID = "specimenID";
+    public static final String SAMPLE_TYPE_CODE = "specimenType_code";
+    public static final String ANATOMIC_SOURCE_CODE = "anatomicSource_code";
+    public static final String LONG_TERM_STROAGE_CONTAINER = "longTermStorageContainer";
+    public static final String STORAGE_TEMPERATURE = "storageTemperature";
+    public static final String PRESERVATION_TYPE_CODE = "preservationType_code";
+    public static final String PARTICIPANT_AGE_UNIT = "participantAge_unit";
+    public static final String PARTICIPANT_AGE = "participantAge_value";
+    public static final String SPECIMEN_COUNT = "specimenCount";
 
-	private Map<String, String> attributeToOntologyUri;
-	{
-		attributeToOntologyUri = new HashMap<String, String>();
-		attributeToOntologyUri.put(SAMPLE_ID, RdfExporter.SAMPLE_PREFIX);
-		attributeToOntologyUri.put(SAMPLE_TYPE_CODE, RdfExporter.NCI_PREFIX);
-		attributeToOntologyUri.put(ANATOMIC_SOURCE_CODE, RdfExporter.NCI_PREFIX);
-		attributeToOntologyUri.put(PRESERVATION_TYPE_CODE, RdfExporter.NCI_PREFIX);
-	}
+    private static final List<String> columns = Arrays.asList(SAMPLE_ID, SAMPLE_TYPE_CODE, SAMPLE_TYPE_CODE,
+            LONG_TERM_STROAGE_CONTAINER, STORAGE_TEMPERATURE, PRESERVATION_TYPE_CODE, PARTICIPANT_AGE_UNIT,
+            PARTICIPANT_AGE, SPECIMEN_COUNT);
 
-	private Map<String, Property> attributeProperty;
-	{
-		attributeProperty = new HashMap<String, Property>();
-		attributeProperty.put(SAMPLE_TYPE_CODE, RDFS.seeAlso);
-		attributeProperty.put(ANATOMIC_SOURCE_CODE, RDFS.seeAlso);
-		attributeProperty.put(LONG_TERM_STROAGE_CONTAINER, RDFS.seeAlso);
-		attributeProperty.put(STORAGE_TEMPERATURE, RDFS.seeAlso);
-		attributeProperty.put(PRESERVATION_TYPE_CODE, RDFS.seeAlso);
-		attributeProperty.put(PARTICIPANT_AGE_UNIT, RDFS.seeAlso);
-		attributeProperty.put(PARTICIPANT_AGE, RDFS.seeAlso);
-		attributeProperty.put(SPECIMEN_COUNT, RDFS.seeAlso);
-	}
+    private final Map<String, String> attributeToOntologyUri;
 
-	private Map<String, XSDDatatype> attrToDataType;
-	{
-		attrToDataType = new HashMap<String, XSDDatatype>();
-		attrToDataType.put(SAMPLE_ID, XSDDatatype.XSDstring);
-		attrToDataType.put(SAMPLE_TYPE_CODE, XSDDatatype.XSDstring);
-		attrToDataType.put(LONG_TERM_STROAGE_CONTAINER, XSDDatatype.XSDstring);
-		attrToDataType.put(STORAGE_TEMPERATURE, XSDDatatype.XSDinteger);
-		attrToDataType.put(ANATOMIC_SOURCE_CODE, XSDDatatype.XSDstring);
-		attrToDataType.put(PRESERVATION_TYPE_CODE, XSDDatatype.XSDstring);
-		attrToDataType.put(PRESERVATION_TYPE_CODE, XSDDatatype.XSDstring);
-		attrToDataType.put(PARTICIPANT_AGE_UNIT, XSDDatatype.XSDstring);
-		attrToDataType.put(PARTICIPANT_AGE, XSDDatatype.XSDinteger);
-		attrToDataType.put(SPECIMEN_COUNT, XSDDatatype.XSDinteger);
-	}
+    {
+        attributeToOntologyUri = new HashMap<>();
+        attributeToOntologyUri.put(SAMPLE_ID, RdfExporter.SAMPLE_PREFIX);
+        attributeToOntologyUri.put(SAMPLE_TYPE_CODE, RdfExporter.NCI_PREFIX);
+        attributeToOntologyUri.put(ANATOMIC_SOURCE_CODE, RdfExporter.NCI_PREFIX);
+        attributeToOntologyUri.put(PRESERVATION_TYPE_CODE, RdfExporter.NCI_PREFIX);
+    }
 
-	public String getIdentifierAttr()
-	{
-		return SAMPLE_ID;
-	}
+    private final Map<String, Property> attributeProperty;
 
-	public XSDDatatype getDataType(String attributeName)
-	{
-		return attrToDataType.containsKey(attributeName) ? attrToDataType.get(attributeName) : XSDDatatype.XSDstring;
-	}
+    {
+        attributeProperty = new HashMap<>();
+        attributeProperty.put(SAMPLE_TYPE_CODE, RDFS.seeAlso);
+        attributeProperty.put(ANATOMIC_SOURCE_CODE, RDFS.seeAlso);
+        attributeProperty.put(LONG_TERM_STROAGE_CONTAINER, RDFS.seeAlso);
+        attributeProperty.put(STORAGE_TEMPERATURE, RDFS.seeAlso);
+        attributeProperty.put(PRESERVATION_TYPE_CODE, RDFS.seeAlso);
+        attributeProperty.put(PARTICIPANT_AGE_UNIT, RDFS.seeAlso);
+        attributeProperty.put(PARTICIPANT_AGE, RDFS.seeAlso);
+        attributeProperty.put(SPECIMEN_COUNT, RDFS.seeAlso);
+    }
 
-	public String getOntologyPrefix(String attributeName)
-	{
-		return attributeToOntologyUri.containsKey(attributeName) ? attributeToOntologyUri.get(attributeName) : null;
-	}
+    private final Map<String, XSDDatatype> attrToDataType;
 
-	public Property getAttrProperty(String attributeName)
-	{
-		return attributeProperty.containsKey(attributeName) ? attributeProperty.get(attributeName) : null;
-	}
+    {
+        attrToDataType = new HashMap<>();
+        attrToDataType.put(SAMPLE_ID, XSDDatatype.XSDstring);
+        attrToDataType.put(SAMPLE_TYPE_CODE, XSDDatatype.XSDstring);
+        attrToDataType.put(LONG_TERM_STROAGE_CONTAINER, XSDDatatype.XSDstring);
+        attrToDataType.put(STORAGE_TEMPERATURE, XSDDatatype.XSDinteger);
+        attrToDataType.put(ANATOMIC_SOURCE_CODE, XSDDatatype.XSDstring);
+        attrToDataType.put(PRESERVATION_TYPE_CODE, XSDDatatype.XSDstring);
+        attrToDataType.put(PRESERVATION_TYPE_CODE, XSDDatatype.XSDstring);
+        attrToDataType.put(PARTICIPANT_AGE_UNIT, XSDDatatype.XSDstring);
+        attrToDataType.put(PARTICIPANT_AGE, XSDDatatype.XSDinteger);
+        attrToDataType.put(SPECIMEN_COUNT, XSDDatatype.XSDinteger);
+    }
 
-	public List<String> getAttributeNames()
-	{
-		return columns;
-	}
+    @Override
+    public String getIdentifierAttr() {
+        return SAMPLE_ID;
+    }
+
+    @Override
+    public XSDDatatype getDataType(String attributeName) {
+        return attrToDataType.containsKey(attributeName) ? attrToDataType.get(attributeName) : XSDDatatype.XSDstring;
+    }
+
+    @Override
+    public String getOntologyPrefix(String attributeName) {
+        return attributeToOntologyUri.get(attributeName);
+    }
+
+    @Override
+    public Property getAttrProperty(String attributeName) {
+        return attributeProperty.get(attributeName);
+    }
+
+    @Override
+    public List<String> getAttributeNames() {
+        return columns;
+    }
 }
